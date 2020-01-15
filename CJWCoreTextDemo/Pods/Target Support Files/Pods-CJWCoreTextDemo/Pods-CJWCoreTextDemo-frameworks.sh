dad35acc832b63248a5204ca4bf7099bfc35511a
a7340103 <a73401032016@163.com>
2020-01-15 09:36:13 +0800

    @update 添加ctcoretext

--- CJWCoreTextDemo/Pods/Target Support Files/Pods-CJWCoreTextDemo/Pods-CJWCoreTextDemo-frameworks.sh
+++ CJWCoreTextDemo/Pods/Target Support Files/Pods-CJWCoreTextDemo/Pods-CJWCoreTextDemo-frameworks.sh
@@ -161,9 +161,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/DTCoreText/DTCoreText.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DTFoundation/DTFoundation.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/DTCoreText/DTCoreText.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DTFoundation/DTFoundation.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
